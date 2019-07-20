5fd95a8336f2b6d378d394fd49f42febe3219733
venkatakrishna-inti <venkatakrishnainti@gmail.com>
2019-07-20 16:52:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,2 @@
+var logger = require('./logger');
+console.log("logger",logger);
---
+++ logger.js
@@ -0,0 +1,7 @@
+
+var url = "http://mylogger.io/log";
+ function log(message) {
+ 	console.log("message",message)
+ }
+
+ module.exports.log = log;
